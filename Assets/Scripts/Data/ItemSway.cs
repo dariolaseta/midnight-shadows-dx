d728189f7f1e77a9732a6e1ada5186feca27642a
Dario La Seta <dario.laseta@gmail.com>
2024-08-10 17:15:38 +0200

    added item sway

---
+++ Assets/Scripts/Data/ItemSway.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSway : MonoBehaviour
+{
+    [SerializeField] float smooth;
+    [SerializeField] float swayMultiplier;
+
+    void Update() {
+        
+        SwayItem();
+    }
+
+    private void SwayItem() {
+
+        if (GameController.Instance.State != GameState.FREEROAM) return;
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplier;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplier;
+        
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+    }
+}
