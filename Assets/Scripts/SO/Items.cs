740836b081e385fc197c0c59c562c0c0c5f02025
Dario La Seta <dario.laseta@gmail.com>
2024-11-29 16:17:23 +0100

    added itemtype

--- Assets/Scripts/SO/Items.cs
+++ Assets/Scripts/SO/Items.cs
@@ -9,19 +9,28 @@ public class Items : ScriptableObject
     [SerializeField] string itemName;
     [SerializeField] string itemDescription;
 
+    [SerializeField] int value;
+
     [SerializeField] Sprite itemImage;
 
     [SerializeField] GameObject model;
 
+    [SerializeField] ItemType itemType;
+
+    public enum ItemType
+    {
+        Shrimp,
+        Heal
+    }
+
     public string ItemName => itemName;
     public string ItemDescription => itemDescription;
-
+    
+    public int Value => value;
+    
     public Sprite ItemImage => itemImage;
 
     public GameObject Model => model;
-
-    public enum ItemType {
-
-        Shrimp,
-    }
+    
+    public ItemType Type => itemType;
 }
